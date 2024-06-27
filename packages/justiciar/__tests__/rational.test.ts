68703a02708889b3d2fb72088c5869655e8761c4
Jeremy Banka <hello@jeremybanka.com>
2024-06-26 18:00:13 -0700

    ✨ division, consolidation, and simplification for Rational number

--- packages/justiciar/__tests__/rational.test.ts
+++ packages/justiciar/__tests__/rational.test.ts
@@ -2,11 +2,41 @@ import { describe, it, expect } from "vitest";
 import { Rational } from "../src/rational";
 
 describe("Rational", () => {
-	it("should return the correct value", () => {
+	it("addition and comparison", () => {
 		const a = new Rational(1n, 2n);
 		const b = new Rational(1n, 3n);
-		expect(Rational.isGreaterThan(a).than(b)).toBe(true);
+		expect(a.isGreaterThan(b)).toBe(true);
 		b.add(1n, 3n);
-		expect(Rational.isGreaterThan(a).than(b)).toBe(false);
+		expect(a.isGreaterThan(b)).toBe(false);
+	});
+	test("division", () => {
+		const a = new Rational(1n, 2n);
+		const b = new Rational(1n, 3n);
+		expect(a.divideBy(...b.consolidate())).toStrictEqual(new Rational(3n, 2n));
+	});
+	test("consolidation and simplification", () => {
+		const a = new Rational(1n, 2n);
+		let denominator = 2n;
+		const inc = () => {
+			denominator += 1n;
+			const result = a.add(1n, denominator).simplify();
+			return result;
+		};
+		expect(inc()).toStrictEqual([5n, 6n]);
+		expect(inc()).toStrictEqual([13n, 12n]);
+		expect(inc()).toStrictEqual([77n, 60n]);
+		expect(inc()).toStrictEqual([29n, 20n]);
+		expect(inc()).toStrictEqual([223n, 140n]);
+		expect(inc()).toStrictEqual([481n, 280n]);
+		expect(inc()).toStrictEqual([4609n, 2520n]);
+		expect(inc()).toStrictEqual([4861n, 2520n]);
+		expect(inc()).toStrictEqual([55991n, 27720n]);
+		expect(inc()).toStrictEqual([58301n, 27720n]);
+		expect(inc()).toStrictEqual([785633n, 360360n]);
+		expect(inc()).toStrictEqual([811373n, 360360n]);
+		expect(inc()).toStrictEqual([835397n, 360360n]);
+		expect(inc()).toStrictEqual([1715839n, 720720n]);
+		expect(inc()).toStrictEqual([29889983n, 12252240n]);
+		expect(inc()).toStrictEqual([10190221n, 4084080n]);
 	});
 });
--- packages/justiciar/src/rational.ts
+++ packages/justiciar/src/rational.ts
@@ -5,11 +5,8 @@ export class Rational {
 	protected fractionalValues = new Map<bigint, bigint>();
 
 	protected toFloat(): number {
-		let total = 0;
-		for (const [denominator, numerator] of this.entries()) {
-			total += Number(numerator) / Number(denominator);
-		}
-		return total;
+		const [numerator, denominator] = this.consolidate();
+		return Number(numerator) / Number(denominator);
 	}
 
 	public constructor(numerator?: bigint, denominator = 1n) {
@@ -18,20 +15,125 @@ export class Rational {
 		}
 	}
 
-	public add(numerator: bigint, denominator: bigint): void {
+	public add(that: Rational): Rational;
+	public add(numerator: bigint, denominator: bigint): Rational;
+	public add(p0: bigint | Rational, p1 = 1n): Rational {
+		let numerator: bigint;
+		let denominator = p1;
+		if (p0 instanceof Rational) {
+			[numerator, denominator] = p0.consolidate();
+		} else {
+			numerator = p0;
+		}
 		let newNumerator = numerator;
 		const oldNumerator = this.fractionalValues.get(denominator);
 		if (oldNumerator) {
 			newNumerator = oldNumerator + numerator;
 		}
 		this.fractionalValues.set(denominator, newNumerator);
+		return this;
+	}
+
+	public divideBy(that: Rational): Rational;
+	public divideBy(otherNumerator: bigint, otherDenominator: bigint): Rational;
+	public divideBy(p0: bigint | Rational, p1 = 1n): Rational {
+		let otherNumerator: bigint;
+		let otherDenominator = p1;
+		if (p0 instanceof Rational) {
+			[otherNumerator, otherDenominator] = p0.consolidate();
+		} else {
+			otherNumerator = p0;
+		}
+		const previousEntries = [...this.entries()];
+		this.fractionalValues.clear();
+		for (const [denominator, numerator] of previousEntries) {
+			this.fractionalValues.set(
+				denominator * otherNumerator,
+				numerator * otherDenominator,
+			);
+		}
+		return this;
 	}
 
 	public entries(): IterableIterator<[bigint, bigint]> {
 		return this.fractionalValues.entries();
 	}
 
-	public isGreaterThan(that: Rational) {
+	public consolidate(): [numerator: bigint, denominator: bigint] {
+		let commonFactors = new PrimeFactors();
+		for (const entry of this.entries()) {
+			for (const member of entry) {
+				if (member !== 0n && member !== 1n) {
+					commonFactors = commonFactors.and(member);
+				}
+			}
+		}
+		const condensedDenominator = commonFactors.compute();
+		let condensedNumerator = 0n;
+		for (const [denominator, numerator] of this.fractionalValues.entries()) {
+			condensedNumerator += (numerator * condensedDenominator) / denominator;
+		}
+		return [condensedNumerator, condensedDenominator];
+	}
+
+	public simplify(): [numerator: bigint, denominator: bigint] {
+		const [numerator, denominator] = this.consolidate();
+		const commonFactors = new PrimeFactors(denominator).and(numerator);
+		const condensedDenominator = commonFactors.compute();
+		return [
+			condensedDenominator / denominator,
+			condensedDenominator / numerator,
+		];
+	}
+
+	public isGreaterThan(that: Rational): boolean {
 		return this.toFloat() > that.toFloat();
 	}
+}
+
+export class PrimeFactors {
+	private factors: Map<bigint, bigint>;
+	constructor(n?: bigint) {
+		this.factors = new Map<bigint, bigint>();
+		if (n) {
+			getPrimeFactors(n, this.factors);
+		}
+	}
+	public and(n: bigint): PrimeFactors {
+		const newFactors = getPrimeFactors(n);
+		for (const [newFactor, newCount] of newFactors) {
+			const existingFactorCount = this.factors.get(newFactor);
+			if (!existingFactorCount || existingFactorCount < newCount) {
+				this.factors.set(newFactor, newCount);
+			}
+		}
+		return this;
+	}
+	public compute(): bigint {
+		let result = 1n;
+		for (const [factor, count] of this.factors) {
+			result *= factor ** count;
+		}
+		return result;
+	}
+}
+
+export function getPrimeFactors(
+	n: bigint,
+	factors = new Map<bigint, bigint>(),
+): Map<bigint, bigint> {
+	let i = 2n;
+	while (i <= n) {
+		if (n % i === 0n) {
+			const currentCount = factors.get(i) ?? 0n;
+			factors.set(i, currentCount + 1n);
+			getPrimeFactors(n / i, factors);
+			break;
+		}
+		i += 1n;
+	}
+	if (factors.size === 0) {
+		factors.set(n, 1n);
+	}
+	return factors;
 }
