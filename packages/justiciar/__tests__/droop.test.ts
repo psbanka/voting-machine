687750b3a8e194c8e992f7395f0f932be3c0111a
Jeremy Banka <hello@jeremybanka.com>
2024-07-01 01:57:47 -0700

    ✅ update droop test to new strategy

--- packages/justiciar/__tests__/droop.test.ts
+++ packages/justiciar/__tests__/droop.test.ts
@@ -1,10 +1,11 @@
 import { describe, expect, it } from "vitest"
 
+import { Rational } from "../src"
 import { droopQuota } from "../src/droop"
 describe(`droopQuota`, () => {
 	it(`should return the correct quota`, () => {
-		expect(droopQuota(10n, 1n)).toBe(6n)
-		expect(droopQuota(10n, 2n)).toBe(4n)
-		expect(droopQuota(10n, 3n)).toBe(3n)
+		expect(droopQuota(10n, 1n)).toStrictEqual(new Rational(10n, 2n))
+		expect(droopQuota(10n, 2n)).toStrictEqual(new Rational(10n, 3n))
+		expect(droopQuota(10n, 3n)).toStrictEqual(new Rational(10n, 4n))
 	})
 })
